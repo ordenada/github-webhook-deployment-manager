398c8762b3b3a35e26fe71e8bf06fd87b85745e7
David256 <wasterdavid@gmail.com>
2025-01-17 18:54:30 -0500

    feat: add the main script

---
+++ main.py
@@ -0,0 +1,57 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+
+import os
+import hmac
+import hashlib
+
+from fastapi import FastAPI, Request, HTTPException
+from fastapi.responses import JSONResponse
+import uvicorn
+
+
+app = FastAPI()
+
+SECRET = os.environ['SECRET']
+
+
+def verify_signature(payload_body: bytes, secret_token: str, signature_header: str):
+    if not signature_header:
+        raise HTTPException(
+            status_code=403,
+            detail='x-hub-signature-256 header is missing!',
+        )
+    
+    hash_object = hmac.new(
+        secret_token.encode('utf-8'),
+        msg=payload_body,
+        digestmod=hashlib.sha256,
+    )
+
+    expected_signature = f'sha256={hash_object.hexdigest()}'
+    if not hmac.compare_digest(expected_signature, signature_header):
+        raise HTTPException(
+            status_code=403,
+            detail='Request signatures didn\'t match!',
+        )
+
+
+@app.post('/webhook')
+async def process_webhook(request: Request):
+    signature_header = request.headers.get('x-hub-signature-256')
+    raw = await request.body()
+
+    verify_signature(raw, SECRET, signature_header)
+
+    content_type = request.headers.get('Content-Type')
+    if content_type != 'application/json':
+        raise HTTPException(status_code=415, detail='Content-Type must be application/json')
+
+    data = await request.json()
+    print(data)
+    return JSONResponse(status_code=204)
+
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='localhost', port=3000)
