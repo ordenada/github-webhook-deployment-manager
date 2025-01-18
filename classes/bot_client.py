2919c0702122ee1713adbd13f7c8a8fa72c311a5
David256 <wasterdavid@gmail.com>
2025-01-18 02:26:51 -0500

    feat: add new methods to delete and edit reports

--- classes/bot_client.py
+++ classes/bot_client.py
@@ -5,6 +5,14 @@ from typing import Optional
 from .log import logger
 
 
+TELEGRAM_CHAT_ID = os.environ.get('TELEGRAM_CHAT_ID')
+try:
+    TELEGRAM_CHAT_ID = int(TELEGRAM_CHAT_ID)
+except:
+    pass
+TELEGRAM_THREAD_ID = os.environ.get('TELEGRAM_THREAD_ID')
+
+
 def create_bot_client():
     if os.environ.get('TELEGRAM_BOT_TOKEN') is None:
         logger.error('No set TELEGRAM_BOT_TOKEN')
@@ -20,20 +28,45 @@ def send_report(report: str, markdown: Optional[bool] = None, alert: Optional[bo
     if client is None:
         return
     
-    TELEGRAM_CHAT_ID = os.environ.get('TELEGRAM_CHAT_ID')
     try:
-        TELEGRAM_CHAT_ID = int(TELEGRAM_CHAT_ID)
-    except:
-        pass
-    TELEGRAM_THREAD_ID = os.environ.get('TELEGRAM_THREAD_ID')
-    
-    try:
-        client.send_message(
+        sent_message = client.send_message(
             chat_id=TELEGRAM_CHAT_ID,
             text=report,
             message_thread_id=TELEGRAM_THREAD_ID,
             parse_mode='Markdown' if markdown else None,
             disable_notification=not alert,
+        )
+        return sent_message.message_id
+    except Exception as err:
+        logger.error(err)
+
+
+def edit_report_message(message_id: int, report: str, markdown: Optional[bool] = None):
+    client = create_bot_client()
+    if client is None:
+        return
+
+    try:
+        sent_message = client.edit_message_text(
+            text=report,
+            chat_id=TELEGRAM_CHAT_ID,
+            message_id=message_id,
+            parse_mode='Markdown' if markdown else None,
+        )
+        return sent_message.message_id
+    except Exception as err:
+        logger.error(err)
+
+
+def delete_report_message(message_id: int):
+    client = create_bot_client()
+    if client is None:
+        return
+    
+    try:
+        client.delete_message(
+            chat_id=TELEGRAM_CHAT_ID,
+            message_id=message_id,
         )
     except Exception as err:
         logger.error(err)
